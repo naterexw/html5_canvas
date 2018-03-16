fb2deaab5639761d320d178dd568d7eab46a84d1
Nate <nate@natewelling.com>
2018-03-16 14:57:33 +0800

    start to add bootstrap 4

--- canvas-common.js
+++ canvas-common.js
@@ -1,52 +1,52 @@
-let canvasReal = document.getElementById('canvas-real');
-let contextReal = canvasReal.getContext('2d');
-let canvasDraft = document.getElementById('canvas-draft');
-let contextDraft = canvasDraft.getContext('2d');
+const canvasReal = document.getElementById('canvas-real');
+const contextReal = canvasReal.getContext('2d');
+const canvasDraft = document.getElementById('canvas-draft');
+const contextDraft = canvasDraft.getContext('2d');
 let currentFunction;
 let dragging = false;
 
-$('#canvas-draft').mousedown(function(e){
-    let mouseX = e.pageX - this.offsetLeft;
-    let mouseY = e.pageY - this.offsetTop;
-    currentFunction.onMouseDown([mouseX,mouseY],e);
-    dragging = true;
+$('#canvas-draft').mousedown(e => {
+  const mouseX = e.offsetX;
+  const mouseY = e.offsetY;
+  currentFunction.onMouseDown([mouseX, mouseY], e);
+  dragging = true;
 });
 
-$('#canvas-draft').mousemove(function(e){
-    let mouseX = e.pageX - this.offsetLeft;
-    let mouseY = e.pageY - this.offsetTop;
-    if(dragging){
-        currentFunction.onDragging([mouseX,mouseY],e);
-    }
-    currentFunction.onMouseMove([mouseX,mouseY],e);
+$('#canvas-draft').mousemove(e => {
+  const mouseX = e.offsetX;
+  const mouseY = e.offsetY;
+  if (dragging) {
+    currentFunction.onDragging([mouseX, mouseY], e);
+  }
+  currentFunction.onMouseMove([mouseX, mouseY], e);
 });
 
-$('#canvas-draft').mouseup(function(e){
-    dragging = false;
-    let mouseX = e.pageX - this.offsetLeft;
-    let mouseY = e.pageY - this.offsetTop;
-    currentFunction.onMouseUp([mouseX,mouseY],e);
+$('#canvas-draft').mouseup(e => {
+  dragging = false;
+  const mouseX = e.offsetX;
+  const mouseY = e.offsetY;
+  currentFunction.onMouseUp([mouseX, mouseY], e);
 });
 
-$('#canvas-draft').mouseleave(function(e){
-    dragging = false;
-    let mouseX = e.pageX - this.offsetLeft;
-    let mouseY = e.pageY - this.offsetTop;
-    currentFunction.onMouseLeave([mouseX,mouseY],e);
+$('#canvas-draft').mouseleave(e => {
+  dragging = false;
+  const mouseX = e.offsetX;
+  const mouseY = e.offsetY;
+  currentFunction.onMouseLeave([mouseX, mouseY], e);
 });
 
-$('#canvas-draft').mouseenter(function(e){
-    let mouseX = e.pageX - this.offsetLeft;
-    let mouseY = e.pageY - this.offsetTop;
-    currentFunction.onMouseEnter([mouseX,mouseY],e);
+$('#canvas-draft').mouseenter(e => {
+  const mouseX = e.offsetX;
+  const mouseY = e.offsetY;
+  currentFunction.onMouseEnter([mouseX, mouseY], e);
 });
 
-class PaintFunction{
-    constructor(){}
-    onMouseDown(){}
-    onDragging(){}
-    onMouseMove(){}
-    onMouseUp(){}
-    onMouseLeave(){}
-    onMouseEnter(){}
-}    
+class PaintFunction {
+  constructor() {}
+  onMouseDown() {}
+  onDragging() {}
+  onMouseMove() {}
+  onMouseUp() {}
+  onMouseLeave() {}
+  onMouseEnter() {}
+}
