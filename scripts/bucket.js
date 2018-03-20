df12773a0eb38d925d25024404841efb7b679869
Andrew Cheung <cheungdzinyung@gmail.com>
2018-03-20 17:41:59 +0800

    commit to update local master

--- scripts/bucket.js
+++ scripts/bucket.js
@@ -5,68 +5,8 @@ class Bucket extends PaintFunction {
     }
 
     onMouseDown(coord, event) {
-        let pixelStack = [coord[0], coord[1]];
-
-        while (pixelStack.length) {
-            var newPos, x, y, pixelPos, reachLeft, reachRight;
-            newPos = pixelStack.pop();
-            x = newPos[0];
-            y = newPos[1];
-
-            pixelPos = (y * canvasWidth + x) * 4;
-            while (y-- >= drawingBoundTop && matchStartColor(pixelPos)) {
-                pixelPos -= canvasWidth * 4;
-            }
-            pixelPos += canvasWidth * 4;
-            ++y;
-            reachLeft = false;
-            reachRight = false;
-            while (y++ < canvasHeight - 1 && matchStartColor(pixelPos)) {
-                colorPixel(pixelPos);
-
-                if (x > 0) {
-                    if (matchStartColor(pixelPos - 4)) {
-                        if (!reachLeft) {
-                            pixelStack.push([x - 1, y]);
-                            reachLeft = true;
-                        }
-                    }
-                    else if (reachLeft) {
-                        reachLeft = false;
-                    }
-                }
-
-                if (x < canvasWidth - 1) {
-                    if (matchStartColor(pixelPos + 4)) {
-                        if (!reachRight) {
-                            pixelStack.push([x + 1, y]);
-                            reachRight = true;
-                        }
-                    }
-                    else if (reachRight) {
-                        reachRight = false;
-                    }
-                }
-
-                pixelPos += canvasWidth * 4;
-            }
-        }
-        context.putImageData(colorLayer, 0, 0);
-
-        function matchStartColor(pixelPos) {
-            var r = colorLayer.data[pixelPos];
-            var g = colorLayer.data[pixelPos + 1];
-            var b = colorLayer.data[pixelPos + 2];
-
-            return (r == startR && g == startG && b == startB);
-        }
-
-        function colorPixel(pixelPos) {
-            colorLayer.data[pixelPos] = fillColorR;
-            colorLayer.data[pixelPos + 1] = fillColorG;
-            colorLayer.data[pixelPos + 2] = fillColorB;
-            colorLayer.data[pixelPos + 3] = 255;
-        }
+        let startColor = this.context.getImageData(coord[0], coord[1], 1, 1).data;
+        floodFill(coord[0], coord[1], event);
     }
 
     onDragging() { }
@@ -76,3 +16,68 @@ class Bucket extends PaintFunction {
     onMouseEnter() { }
 }
 
+function floodFill(startX, startY, e) {
+    let pixelStack = [[startX, startY]];
+    while (pixelStack.length) {
+        var newPos, x, y, pixelPos, reachLeft, reachRight;
+        newPos = pixelStack.pop();
+        x = newPos[0];
+        y = newPos[1];
+
+        pixelPos = (y * 800 + x) * 4;
+        while (y-- >= e.offsetX && matchStartColor(pixelPos)) {
+            pixelPos -= 800 * 4;
+        }
+        pixelPos += 800 * 4;
+        ++y;
+        reachLeft = false;
+        reachRight = false;
+        while (y++ < 500 - 1 && matchStartColor(pixelPos)) {
+            colorPixel(pixelPos);
+
+            if (x > 0) {
+                if (matchStartColor(pixelPos - 4)) {
+                    if (!reachLeft) {
+                        pixelStack.push([x - 1, y]);
+                        reachLeft = true;
+                    }
+                }
+                else if (reachLeft) {
+                    reachLeft = false;
+                }
+            }
+
+            if (x < 800 - 1) {
+                if (matchStartColor(pixelPos + 4)) {
+                    if (!reachRight) {
+                        pixelStack.push([x + 1, y]);
+                        reachRight = true;
+                    }
+                }
+                else if (reachRight) {
+                    reachRight = false;
+                }
+            }
+
+            pixelPos += 800 * 4;
+        }
+    }
+    
+    var colorLayer = [0,10,0,255];
+    context.putImageData(colorLayer, 0, 0);
+
+    function matchStartColor(pixelPos) {
+        var r = colorLayer.data[pixelPos];
+        var g = colorLayer.data[pixelPos + 1];
+        var b = colorLayer.data[pixelPos + 2];
+
+        return (r == startR && g == startG && b == startB);
+    }
+
+    function colorPixel(pixelPos) {
+        colorLayer.data[pixelPos] = fillColorR;
+        colorLayer.data[pixelPos + 1] = fillColorG;
+        colorLayer.data[pixelPos + 2] = fillColorB;
+        colorLayer.data[pixelPos + 3] = 255;
+    }
+}
