c322fd8e15cde321ed777964a9aa376a5de0b515
Ivan Oung <ivanoung@gmail.com>
2018-03-21 16:17:40 +0800

    Adding different saving options, adding bluring filter function

--- scripts/canvas-filter.js
+++ scripts/canvas-filter.js
@@ -11,43 +11,46 @@ let canvasFilterSetting = {
     sepia: 0
 };
 
-function checkChangeFilter(){
+function checkChangeFilter() {
+
+    canvasFilterSetting.blur = $('#dw-blur-adj').val();
+    
     canvasFilterSetting.brightness = $('#dw-brightness-adj').val();
-        $("#dw-brightness-dsply").html(canvasFilterSetting.brightness);
+    $("#dw-brightness-dsply").html(canvasFilterSetting.brightness);
 
-        canvasFilterSetting.contrast = $('#dw-contrast-adj').val();
-        $("#dw-contrast-dsply").html(canvasFilterSetting.contrast);
+    canvasFilterSetting.contrast = $('#dw-contrast-adj').val();
+    $("#dw-contrast-dsply").html(canvasFilterSetting.contrast);
 
-        canvasFilterSetting.grayscale = $('#dw-grayscale-adj').val();
-        $("#dw-grayscale-dsply").html(canvasFilterSetting.grayscale);
+    canvasFilterSetting.grayscale = $('#dw-grayscale-adj').val();
+    $("#dw-grayscale-dsply").html(canvasFilterSetting.grayscale);
 
-        canvasFilterSetting.huerotate = $('#dw-huerotate-adj').val();
-        $("#dw-huerotate-dsply").html(canvasFilterSetting.huerotate);
+    canvasFilterSetting.huerotate = $('#dw-huerotate-adj').val();
+    $("#dw-huerotate-dsply").html(canvasFilterSetting.huerotate);
 
-        canvasFilterSetting.invert = $('#dw-invert-adj').val();
-        $("#dw-invert-dsply").html(canvasFilterSetting.invert);
+    canvasFilterSetting.invert = $('#dw-invert-adj').val();
+    $("#dw-invert-dsply").html(canvasFilterSetting.invert);
 
-        canvasFilterSetting.opacity = $('#dw-opacity-adj').val();
-        $("#dw-opacity-dsply").html(canvasFilterSetting.opacity);
+    canvasFilterSetting.opacity = $('#dw-opacity-adj').val();
+    $("#dw-opacity-dsply").html(canvasFilterSetting.opacity);
 
-        canvasFilterSetting.saturate = $('#dw-saturate-adj').val();
-        $("#dw-saturate-dsply").html(canvasFilterSetting.saturate);
+    canvasFilterSetting.saturate = $('#dw-saturate-adj').val();
+    $("#dw-saturate-dsply").html(canvasFilterSetting.saturate);
 
-        canvasFilterSetting.sepia = $('#dw-sepia-adj').val();
-        $("#dw-sepia-dsply").html(canvasFilterSetting.sepia);
+    canvasFilterSetting.sepia = $('#dw-sepia-adj').val();
+    $("#dw-sepia-dsply").html(canvasFilterSetting.sepia);
 
-        canvasFilterSetting.dropshadow[0] = $("#offset-x").val();
-        canvasFilterSetting.dropshadow[1] = $("#offset-y").val();
-        canvasFilterSetting.dropshadow[2] = $("#dps-blur-radius").val();
-        canvasFilterSetting.dropshadow[3] = $("#dps-color").val();
-
-
-        let canvasFilterStringTemplate =
-            `blur(${canvasFilterSetting.blur}px) brightness(${canvasFilterSetting.brightness}%) contrast(${canvasFilterSetting.contrast}%) grayscale(${canvasFilterSetting.grayscale}%) hue-rotate(${canvasFilterSetting.huerotate}deg) invert(${canvasFilterSetting.invert}%)  opacity(${canvasFilterSetting.opacity}%) saturate(${canvasFilterSetting.saturate}%) sepia(${canvasFilterSetting.sepia}%) drop-shadow(${canvasFilterSetting.dropshadow[0]}px ${canvasFilterSetting.dropshadow[1]}px ${canvasFilterSetting.dropshadow[2]}px ${canvasFilterSetting.dropshadow[3]})`;
+    canvasFilterSetting.dropshadow[0] = $("#offset-x").val();
+    canvasFilterSetting.dropshadow[1] = $("#offset-y").val();
+    canvasFilterSetting.dropshadow[2] = $("#dps-blur-radius").val();
+    canvasFilterSetting.dropshadow[3] = $("#dps-color").val();
 
 
-        contextDraft.filter = canvasFilterStringTemplate;
-        contextReal.filter = canvasFilterStringTemplate;
+    let canvasFilterStringTemplate =
+        `blur(${canvasFilterSetting.blur}px) brightness(${canvasFilterSetting.brightness}%) contrast(${canvasFilterSetting.contrast}%) grayscale(${canvasFilterSetting.grayscale}%) hue-rotate(${canvasFilterSetting.huerotate}deg) invert(${canvasFilterSetting.invert}%)  opacity(${canvasFilterSetting.opacity}%) saturate(${canvasFilterSetting.saturate}%) sepia(${canvasFilterSetting.sepia}%) drop-shadow(${canvasFilterSetting.dropshadow[0]}px ${canvasFilterSetting.dropshadow[1]}px ${canvasFilterSetting.dropshadow[2]}px ${canvasFilterSetting.dropshadow[3]})`;
+
+
+    contextDraft.filter = canvasFilterStringTemplate;
+    contextReal.filter = canvasFilterStringTemplate;
 }
 
 
@@ -65,7 +68,7 @@ $('input[type="range"]').on('mousemove', function () {
 
 
 $("[type=reset],[type=range]").click(() => {
-    window.setTimeout(checkChangeFilter,50);
+    window.setTimeout(checkChangeFilter, 50);
 });
 
-$(window).on("keyup",()=> checkChangeFilter());
+$(window).on("keyup", () => checkChangeFilter());
