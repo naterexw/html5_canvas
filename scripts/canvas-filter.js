17c83b9b2b47d5b6c168a10d43a7b593d1833c5a
Ivan Oung <ivanoung@gmail.com>
2018-03-21 12:09:13 +0800

    Fixed current drawing filters in HTML and fixed filter js template
    werigsfiluehlrfhaerliufaehlrg

--- scripts/canvas-filter.js
+++ scripts/canvas-filter.js
@@ -1,42 +1,46 @@
-// $("#blur").on('keyup',function(e){
-
-//     // contextReal.filter = "blur(30px) opacity(0%)";
-//     contextReal.filter = `opacity(${$("#blur").val()}%)`;
-//     // $('.canvas').css("filter",`opacity(${$("#blur").val()}%)`);
-//     // console.log(contextReal);
-//     // canvasReal.filter = `blur(${10}px)`;
-// });
-
-// $("#blur").val()
-
 let canvasFilterSetting = {
     blur: 0,
     brightness: 100,
     contrast: 100,
-    dropshadow: [0,0,0,"black"],
+    dropshadow: [0, 0, 0, "black"],
     grayscale: 0,
     huerotate: 0,
     invert: 0,
     opacity: 100,
     saturate: 100,
     sepia: 0
-}
+};
+
+
 
 let mousedown = false;
-$(window).on('mousedown',()=>mousedown = true);
-$(window).on('mouseup',()=>mousedown = false);
+$(window).on('mousedown', () => mousedown = true);
+$(window).on('mouseup', () => mousedown = false);
+
+$('#brightness-adjustment').on('mousemove', function () {
+    if (mousedown) {
+        canvasFilterSetting.brightness = $('#brightness-adjustment').val();
+        $("#brightness-degree").html(canvasFilterSetting.brightness);
 
 
+        let canvasFilterStringTemplate =
+            `
+            blur(${canvasFilterSetting.blur}px) 
+            brightness(${canvasFilterSetting.brightness}%) 
+            contrast(${canvasFilterSetting.contrast}%) 
+            grayscale(${canvasFilterSetting.grayscale}%) 
+            hue-rotate(${canvasFilterSetting.huerotate}deg) 
+            invert(${canvasFilterSetting.invert}%) 
+            opacity(${canvasFilterSetting.opacity}%) 
+            saturate(${canvasFilterSetting.saturate}%) 
+            sepia(${canvasFilterSetting.sepia}%)
+            `;
 
-$('#brightness-adjustment').on('mousemove',function(){
-    if (mousedown){
-        let brightness = $('#brightness-adjustment').val();
-        $("#brightness-degree").html(brightness);
-        contextDraft.filter = `brightness(${brightness}%)`;
-        contextReal.filter = `brightness(${brightness}%)`;
+        contextDraft.filter = canvasFilterStringTemplate;
+        contextReal.filter = canvasFilterStringTemplate;
     }
-    
+
 })
 
 
-`blur(10px) brightness(100%) contrast(100%) drop-shadow(1px 1px 1px red) grayscale(0%) hue-rotate(0deg) invert(0%) opacity(100%) saturate(100%) sepia(0%)`
+// drop-shadow(${canvasFilterSetting.dropshadow})
