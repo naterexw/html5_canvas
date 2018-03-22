f667dcb1d2ed5a4f1cc91f9e1515a4ef2d405bb0
Ivan Oung <ivanoung@gmail.com>
2018-03-22 10:55:05 +0800

    Hooking up color picker to style variables

--- scripts/color-picker.js
+++ scripts/color-picker.js
@@ -1,14 +1,32 @@
 // Color picker
 
-$('#custom').spectrum({
+$('#fill').spectrum({
   flat: false,
   showInput: true,
-
   change: color => {
     let col = color.toHexString();  
     canvasSettings.curCol.fill = col;
+    $('[value=fill]').css('background-color', col);
+  },
+});
+
+$('#stroke').spectrum({
+  flat: false,
+  showInput: true,
+  change: color => {
+    let col = color.toHexString();  
     canvasSettings.curCol.stroke = col;
-    console.log(col);
-    $('#fill').css('background-color', col);
+    $('[value=stroke]').css('background-color', col);
+  },
+});
+
+$('#bgc').spectrum({
+  flat: false,
+  showInput: true,
+  change: color => {
+    let col = color.toHexString();  
+    canvasSettings.curCol.bgc = col;
+    $('[value=stroke]').css('background-color', col);
+    $('#canvas-real').css('background-color', col);
   },
 });
