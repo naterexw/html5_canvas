5940894a36053de2ee0e1b1fc7080adaf6794367
Nate <nate@natewelling.com>
2018-03-23 09:54:09 +0800

    refactor sidebar flex-box alignment & focus pointer toggle

--- scripts/canvas-scale-ruler.js
+++ scripts/canvas-scale-ruler.js
@@ -1,46 +1,46 @@
 let gridFocusDisplay = false;
-$(document).ready(()=>{
-    $("#canvas-indicator").hide();
-    $("#focus").click(()=>{
-        if (gridFocusDisplay){
-            $("#canvas-indicator").hide();
-            gridFocusDisplay = false;
-        }
-        else{
-            $("#canvas-indicator").show();
-            gridFocusDisplay = true;
-        }
-    });
-    $(".canvas").on('mousemove', function(e){
-        if(gridFocusDisplay){
-            const locOfMouse = [e.offsetX, e.offsetY];
-            drawFocus(canvasIndicator, contextIndicator, locOfMouse);
-        }
-    });
+
+$(document).ready(() => {
+  $('#canvas-indicator').hide();
+  $('#focus').click(() => {
+    if (gridFocusDisplay) {
+      $('#canvas-indicator').hide();
+      gridFocusDisplay = false;
+    } else {
+      $('#canvas-indicator').show();
+      gridFocusDisplay = true;
+    }
+  });
+  $('.canvas').on('mousemove', e => {
+    if (gridFocusDisplay) {
+      const locOfMouse = [e.offsetX, e.offsetY];
+      drawFocus(canvasIndicator, contextIndicator, locOfMouse);
+    }
+  });
 });
 
-function drawFocus (canvas,context, xyloc){
-    // console.log(xyloc);
-    context.clearRect(0,0, canvas.width, canvas.height);
-    context.strokeStyle = canvasSettings.curCol.stroke;
+function drawFocus(canvas, context, xyloc) {
+  // console.log(xyloc);
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.strokeStyle = canvasSettings.curCol.stroke;
 
-    // Horizontal path
-    context.beginPath();
-    context.moveTo(0,xyloc[1]);
-    context.lineTo(canvas.width, xyloc[1]);
-    context.closePath();
-    context.stroke();
+  // Horizontal path
+  context.beginPath();
+  context.moveTo(0, xyloc[1]);
+  context.lineTo(canvas.width, xyloc[1]);
+  context.closePath();
+  context.stroke();
 
-    // Vertical
-    context.beginPath();
-    context.moveTo(xyloc[0], canvas.height);
-    context.lineTo(xyloc[0], 0);
-    context.closePath();
-    context.stroke();
+  // Vertical
+  context.beginPath();
+  context.moveTo(xyloc[0], canvas.height);
+  context.lineTo(xyloc[0], 0);
+  context.closePath();
+  context.stroke();
 
-    // Draw target circle
-    context.beginPath();
-    context.arc(xyloc[0], xyloc[1], canvas.width/350, 0, 2 * Math.PI, false);
-    context.closePath();
-    context.stroke();
+  // Draw target circle
+  context.beginPath();
+  context.arc(xyloc[0], xyloc[1], canvas.width / 350, 0, 2 * Math.PI, false);
+  context.closePath();
+  context.stroke();
 }
